cc9b44b0e6ab926ebac57b3b3ab75d6b4ff52b8f
kenstam <ken.stam@versapay.com>
2019-10-10 13:35:13 -0400

    sFTP testing too

--- ftpserver/scripts/bootstrap.sh
+++ ftpserver/scripts/bootstrap.sh
@@ -7,3 +7,8 @@ sudo apt-get -y autoremove
 sudo ufw disable
 
 sudo apt-get -y install vsftpd
+
+# allow password sftp
+sudo cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+sudo cp /home/vagrant/sshd_config /etc/ssh/sshd_config
+sudo service ssh restart
