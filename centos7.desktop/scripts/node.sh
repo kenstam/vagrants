2eeaef11c71312fd42a28c9b4690c7cc90d277e8
kenstam <ken.stam@versapay.com>
2021-04-01 21:19:46 -0400

    cleanup

--- centos7.desktop/scripts/node.sh
+++
@@ -1,2 +0,0 @@
-curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
-sudo yum -y install nodejs yarn
--- ubuntu1404.desktop/scripts/ruby.sh
+++
@@ -1,6 +0,0 @@
-apt-get install build-essential software-properties-common
-apt-add-repository ppa:brightbox/ruby-ng
-apt-get update
-apt-get install -y ruby2.3 ruby2.3-dev ruby-switch
-ruby-switch --set ruby2.3
-gem install bundler --no-ri --no-rdoc
